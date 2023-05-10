b2ed05beda855365ac44ad66f9a7691863a6b142
Nguyenpham12 <nguyenpham6273@gmail.com>
2023-05-10 23:19:07 +0700

    add button

--- BTTH01/QLSV/Student.php
+++ BTTH01/QLSV/Student.php
@@ -5,25 +5,25 @@
         private $age;
         private $grade;
 
-        public function __construct(string $id,string $name,int $age,float $grade) {
+        public function __construct($id,$name,$age,$grade) {
             $this->id = $id; // id la thuoc tinh tren private
             $this->name = $name;
             $this->age = $age;
             $this->grade = $grade;
         }
 
-        public function get_ID():string{
+        public function get_ID(){
             return $this->id;
         }
 
-        public function get_Name():string{
+        public function get_Name(){
             return $this->name;
         }
-        public function get_Age():int{
+        public function get_Age(){
             return $this->age;
         }
 
-        public function get_Grade():float{  
+        public function get_Grade(){  
             return $this->grade;
         }
 
--- BTTH01/QLSV/StudentDAO.php
+++ BTTH01/QLSV/StudentDAO.php
@@ -16,22 +16,44 @@
             // Tạo danh sách sinh viên
             $students = array();
             foreach ($data as $row) {
-            $student = new Student($row[0], $row[1], $row[2], $row[3]);
-            array_push($students, $student);
+                if ($row[0] !== null) {
+                    $student = new Student($row[0], $row[1], $row[2], $row[3]);
+                    array_push($students, $student);
+                }
         }
 
-        // Trả về danh sách sinh viên
         return $students;
     }
-        
-    public function delete($index){
-        unset($student[$index]);
+    public function add(Student $student) {
+        $handle = fopen($this->filename, 'a');
+        $row = array($student->get_ID(), $student->get_Name(), $student->get_Age(), $student->get_Grade());
+        fputcsv($handle, $row);
+        fclose($handle);
     }
+}
+
+// Add student form
+echo '<form method="POST">';
+echo '<label>ID:</label><input type="text" name="id">';
+echo '<label>Name:</label><input type="text" name="name">';
+echo '<label>Age:</label><input type="text" name="age">';
+echo '<label>Grade:</label><input type="text" name="grade">';
+echo '<input type="submit" name="add" value="Add">';
+echo '</form>';
+
+// Bind add button click event
+if (isset($_POST['add'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $grade = $_POST['grade'];
+
+    $student = new Student($id, $name, $age, $grade);
+    $dao = new StudentDAO('../data/Student.csv');
+    $dao->add($student);
     
-        
-        
-       
     }
+
     
 
 ?>

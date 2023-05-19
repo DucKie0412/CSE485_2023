4de0e144152773d7c100510713cd9219c98c25b3
Nguyenpham12 <nguyenpham6273@gmail.com>
2023-05-19 16:12:45 +0700

    do du lieu course len trang homepage

--- BTTH02/SAS/views/homepage/homepage.php
+++ BTTH02/SAS/views/homepage/homepage.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,33 +9,31 @@
   <h1>Hệ thống điểm danh cây nhà lá vườn</h1>
   <p>Chào mừng đến với trang điểm danh</p>
     <p>Vui lòng chọn khóa học để tiếp tục!!</p>
-
     <div class="grid-container">
-    <div class="course-card">
       <a href="../loginpage/loginPage.php">
-        <img src="course1.jpg" alt="Khóa học 1" class="course-image">
-        <h2 class="course-title">Khóa học 1</h2>
-        <p class="course-description">Mô tả khóa học 1.</p>
-      </a>
-    </div>
+      <?php
+        require_once '../../models/CourseModel.php';
 
-    <div class="course-card">
-      <a href="../loginpage/loginPage.php">
-        <img src="course2.jpg" alt="Khóa học 2" class="course-image">
-        <h2 class="course-title">Khóa học 2</h2>
-        <p class="course-description">Mô tả khóa học 2.</p>
-      </a>
-    </div>
+        $courseModel = new CourseModel();
 
-    <div class="course-card">
-      <a href="../loginpage/loginPage.php">
-        <img src="course3.jpg" alt="Khóa học 3" class="course-image">
-        <h2 class="course-title">Khóa học 3</h2>
-        <p class="course-description">Mô tả khóa học 3.</p>
-      </a>
-    </div>
+        // Lấy danh sách tất cả các khóa học
+        $courses = $courseModel->getAllCourses();
 
-    <!-- Thêm các khóa học khác tương tự -->
+        // Hiển thị tên khóa học
+        foreach ($courses as $course) {
+     
+          $courseId = $course['id_Course'];
+          $csd = $course['name'];
+
+          echo '<h2>' . $courseId . '</h2>';
+          echo '<p>' . $csd . '</p>';
+        }
+      ?>
+    </a>
+    </div>
+    
+    
   </div>
 </body>
-</html>
+</html> 
+
